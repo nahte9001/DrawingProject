4d031feb2df0e0b6242ca678f35caaa8ab37dc55
Ethan Torres <nahte9001@gmail.com>
2018-11-01 09:17:45 -0600

    added code to draw a stick figure

--- DrawingProject/View/DrawingView.swift
+++ DrawingProject/View/DrawingView.swift
@@ -17,11 +17,30 @@ public class DrawingView: UIView
     public override func draw(_ rect: CGRect) -> Void
     {
         // Drawing code
+        createStickFigure().stroke()
     }
     
     private func createStickFigure() -> UIBezierPath
     {
         let figure : UIBezierPath = UIBezierPath()
+        
+        UIColor.magenta.setStroke()
+        figure.lineWidth = 3.0
+        
+        figure.addArc(withCenter: CGPoint(x: 200, y: 200),
+                      radius: CGFloat(22.2),
+                      startAngle: CGFloat(0),
+                      endAngle: CGFloat(2) * CGFloat.pi,
+                      clockwise: true)
+        
+        figure.move(to: CGPoint(x: 200, y: 220))
+        figure.addLine(to: CGPoint(x:200, y: 270))
+        figure.move(to: CGPoint(x:180, y: 240))
+        figure.addLine(to: CGPoint(x: 220, y: 240))
+        figure.move(to: CGPoint(x: 200, y: 270))
+        figure.addLine(to:CGPoint(x: 180, y: 300))
+        figure.move(to: CGPoint(x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 220, y: 300))
         
         
         return figure
